a5dd5000a43d8d62f33672722590f32585d5675c
Linden Daniels <lindendaniels@gmail.com>
2019-06-23 01:36:10 -0400

    cleaned up code

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function handleNavBar() {
-    let x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
+	let x = document.getElementById("myTopnav");
+	if(x.className === "topnav") {
+		x.className += " responsive";
+	} else {
+		x.className = "topnav";
+	}
+}
